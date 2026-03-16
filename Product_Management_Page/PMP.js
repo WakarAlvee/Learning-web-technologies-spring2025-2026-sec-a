8231ec22729b6b6e97403edeba5308c89cbc5964
Wakar Alvee <walvee10@gmail.com>
2026-03-16 14:44:39 +0600

    Js lab 4 task push

---
+++ Product_Management_Page/PMP.js
@@ -0,0 +1,26 @@
+
+const unitPrice = 1000;
+
+let quantityInput = document.getElementById("quantity");
+const totalDisplay = document.getElementById("total");
+
+quantityInput.addEventListener("input", function(){
+
+    let quantity = parseInt(quantityInput.value);
+
+    // prevent negative value
+    if(quantity < 0){
+        alert("Quantity cannot be negative");
+        quantity = 0;
+        quantityInput.value = 0;
+    }
+
+    let total = unitPrice * quantity;
+
+    totalDisplay.value = total;
+
+    if(total > 1000){
+        alert("Congratulations! You are eligible for a Gift Coupon!");
+    }
+
+});
