e5deccbaeee40a0b0f91fea649d486a53760ab50
Wakar Alvee <walvee10@gmail.com>
2026-04-08 17:12:50 +0600

    Final lab task started

---
+++ FINAL_TERM_WORKS/Lab_task1_final/task1.php
@@ -0,0 +1,12 @@
+<?php
+
+    $length = 20, $width = 10;
+
+    $area = $length * $width;
+    $perimeter = 2 * ($length+$width);
+
+    echo " The area of a Rectangle = {$area}<br>";
+    echo "The area of a Perimeter = ".$perimeter;
+
+
+?>
