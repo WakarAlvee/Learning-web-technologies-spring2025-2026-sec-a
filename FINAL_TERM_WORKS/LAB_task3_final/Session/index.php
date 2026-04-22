f9360adfdb929d53aacf3c0d8cf68a8e4a1cecf1
Wakar Alvee <walvee10@gmail.com>
2026-04-22 16:36:19 +0600

    opned needed file | completed index page

---
+++ FINAL_TERM_WORKS/LAB_task3_final/Session/index.php
@@ -0,0 +1,36 @@
+<?php
+// index.php
+// Public Home Page
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>xCompany - Home</title>
+</head>
+<body>
+    <fieldset>
+    
+    <table width="100%">
+        <tr>
+            <td>
+                <h1>  <span>  X </span>Company</h1> </td>
+
+            <td align="right">
+                <a href="index.php">Home</a> | 
+                <a href="login.php">Login</a> | 
+                <a href="registration.php">Registration</a> </td>
+        </tr>
+    </table>
+    <hr>
+    
+
+    <h4>Welcome to xCompany</h4> 
+    
+ <hr>
+   
+        <center>
+            <p>Copyright &copy; 2017</p>
+</center>
+    
+</body>
+</html>
